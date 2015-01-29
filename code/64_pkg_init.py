627209f954a94adfddcf05b860bdc7c09278f1a6
Danilo de Jesus da Silva Bellini <danilo.bellini@gmail.com>
2015-01-29 08:11:48 -0200

    Add 64_pkg_init.py

---
+++ code/64_pkg_init.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created on Thu Sep 25 17:49:57 2014
+# @author: Danilo de Jesus da Silva Bellini
+
+if "__path__" in locals():
+  print("Imported package {}\n  from this path: {}\n  filename: {}"
+        .format(__name__, __path__[0], __file__))
+else:
+  print("Imported module {}\n  filename: {}".format(__name__, __file__))
