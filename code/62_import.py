9cae0c74384de56b86f683539c84d614c3102604
Danilo de Jesus da Silva Bellini <danilo.bellini@gmail.com>
2015-01-29 05:20:27 -0200

    Add 62_import.py

---
+++ code/62_import.py
@@ -0,0 +1,4 @@
+print(__import__("sys").version_info)
+# sys.version_info(major=2, minor=7, micro=8, releaselevel='final', serial=0)
+# ou
+# sys.version_info(major=3, minor=4, micro=2, releaselevel='final', serial=0)
