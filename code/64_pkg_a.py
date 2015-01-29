96024c8af44225537f466e96d02731212590a46f
Danilo de Jesus da Silva Bellini <danilo.bellini@gmail.com>
2015-01-29 08:11:33 -0200

    Add 64_pkg_a.py

---
+++ code/64_pkg_a.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created on Thu Sep 25 17:51:01 2014
+# @author: Danilo de Jesus da Silva Bellini
+
+if "__path__" in locals():
+  print("Imported package {}\n  from this path: {}\n  filename: {}"
+        .format(__name__, __path__[0], __file__))
+else:
+  print("Imported module {}\n  filename: {}".format(__name__, __file__))
+
+def hw():
+  print("HW")
